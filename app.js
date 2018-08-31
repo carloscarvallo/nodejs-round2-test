b6cd43a7a60a6f4f4d860ca5d17686989fe18d3c
carloscarvallo <carloscarvallo@protonmail.com>
2018-08-31 03:26:22 -0400

    adding basic general err treatment

--- app.js
+++ app.js
@@ -11,4 +11,23 @@ app.use(express.urlencoded({ extended: false }));
 
 app.use('/', apiRouter);
 
+// Unknown Resource handler
+app.use((_req, _res, next) => {
+  const err = new Error('Not Found');
+  err.status = 404;
+  err.developerMessage = 'Unknown Resource';
+  next(err);
+});
+
+/* eslint no-unused-vars: ["error", { "args": "none" }] */
+app.use((err, _req, res, _next) => {
+  res.status(err.status || 500).send({
+    error: {
+      message: err.message,
+      statusCode: err.status,
+      developerMessage: err.developerMessage || err.message,
+    },
+  });
+});
+
 module.exports = app;
