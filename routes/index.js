ba6bfb5b702362f3b6763f747b364f6defaf9d8b
carloscarvallo <carloscarvallo@protonmail.com>
2018-08-31 03:34:06 -0400

    more basics err treatments for router

--- routes/index.js
+++ routes/index.js
@@ -4,4 +4,23 @@ const router = express.Router();
 
 router.use('/books', require('./books'));
 
+/*
+ * Error treatments
+ */
+const setPayloadError = (res, statusCode, message, developerMessage) => {
+  res.status(statusCode).send({
+    error: {
+      message, statusCode, developerMessage: developerMessage || null,
+    },
+  });
+};
+
+/* eslint no-unused-vars: ["error", { "args": "none" }] */
+router.use((error, req, res, next) => {
+  const hasNameProperty = Object.prototype.hasOwnProperty.call(error, 'name');
+  return setPayloadError(res, 500, error.message, 'Something bad happened');
+});
+
+module.exports = router;
+
 module.exports = router;
